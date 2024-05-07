ecd8e625eeb9764165eb9065374ee7aa1b109791
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-05-07 10:52:59 -0300

    :sparkles: First commit

---
+++ src/main/java/com/fatec/student/entities/Student.java
@@ -0,0 +1,28 @@
+package com.fatec.student.entities;
+
+public class Student {
+    private Integer id;
+    private String nome;
+    private String curso;
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }    
+
+    
+}
